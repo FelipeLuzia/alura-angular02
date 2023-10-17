a8e231fa4525414980bcb081049d4020de68c358
Felipe Luzia <felipe.luzia@hotmail.com>
2023-10-17 10:57:50 -0300

    05 NgClass

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,16 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
   listaPensamentos = [
-    // {
-    //   conteudo: 'Passo informações para o componente filho',
-    //   autoria: 'Componente pai',
-    //   modelo: 'modelo3',
-    // },
-    // {
-    //   conteudo: 'Minha propriedade é decorada com @Input()',
-    //   autoria: 'Componente filho',
-    //   modelo: 'modelo2',
-    // },
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2',
+    },
+    {
+      conteudo:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue finibus libero gravida finibus. Praesent vel nisi nulla. Praesent gravida efficitur tellus, vel dignissim libero. Aenean maximus aliquam mi nec sodales. Mauris ultricies ac odio nec.',
+      autoria: '',
+      modelo: 'modelo1',
+    },
   ];
 
   constructor() {}
